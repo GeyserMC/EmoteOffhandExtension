d07ad2a2571f3b5b49d96e7645913e082317e74a
onebeastchris <github@onechris.mozmail.com>
2025-11-08 11:27:29 +0100

    Add build.yml, include LICENSE in shadowJar

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,32 +8,21 @@ plugins {
 group = "org.geysermc.extension.emoteoffhand"
 version = "1.0.0"
 
-configurations {
-    create("shadowBundle") {
-        isCanBeResolved = true
-        isCanBeConsumed = false
-        isTransitive = false
-    }
-}
-
 repositories {
     mavenCentral()
     maven("https://repo.opencollab.dev/main")
 }
 
-val shadowBundle = configurations.named("shadowBundle")
-
 dependencies {
     compileOnly("org.geysermc.geyser:api:2.9.0-SNAPSHOT")
 
     implementation("org.yaml:snakeyaml:2.5")
-    shadowBundle("org.yaml:snakeyaml:2.5")
+    shadow("org.yaml:snakeyaml:2.5")
 }
 
 tasks {
     named<Jar>("jar") {
         duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-
         archiveFileName.set("${project.name}-${project.version}-nonrelocated.jar")
 
         from({
@@ -44,10 +33,8 @@ tasks {
     }
 
     val shadowJar = named<ShadowJar>("shadowJar") {
-        configurations = listOf(project.configurations.getByName("shadowBundle"))
-
         archiveFileName.set("${project.name}.jar")
-
+        from(project.rootProject.file("LICENSE"))
         relocate("org.yaml.snakeyaml", "org.geysermc.extension.emoteoffhand.shadow.snakeyaml")
     }
 
